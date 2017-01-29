49eaa47eac25750f4c4f39fce30ae0633d73ee8a
Cameron Fulton <rookenthusiast@gmail.com>
2017-01-29 17:53:33 +0000

    back and next page buttons working updating list

--- public/index.js
+++ public/index.js
@@ -1,14 +1,5 @@
 var allCards = null;
-
-var nextPageButton = function(){
-  var clicks = 0;
-  console.log(clicks)
-  clicks += 1;
-  var url = "https://api.magicthegathering.io/v1/cards?page=" + (0 + click);
-  console.log(url);
-  makeRequest(url, requestComplete);
-}
-
+var clicks = 1
 
 var cardsByPage = function(allCards, page, pageNum){
   var ul = document.querySelector("#card_list");
@@ -17,8 +8,11 @@ var cardsByPage = function(allCards, page, pageNum){
   console.log(cards.length);
   cards.forEach(function(card){
     var li = document.createElement("li")
+    var img = document.createElement("img")
     li.innerText = card.name;
+    img.src = card.imageUrl;
     ul.appendChild(li);
+    li.appendChild(img);
   })
 }
 
@@ -35,19 +29,57 @@ var requestComplete = function(){
   var jsonString = this.responseText;
   allCards = JSON.parse(jsonString);
   cardsByPage(allCards);
-  }
+  location.reload;
+}
 
-  var clickTest = function(){
-    console.log("the clicked worked");
+var butttonTest = function(){
+  console.log("the clicked worked");
+}
+
+var backPageButton = function(){
+  console.log(clicks)
+  if (clicks <= 1 ){
+    return;
+  } else {
+    clicks -= 1
+    var url = "https://api.magicthegathering.io/v1/cards?page="+ (clicks);
+    console.log(url);
+    makeRequest(url, requestComplete);
   }
+}
+
+var nextPageButton = function(){
+  console.log(clicks)
+  clicks += 1;
+  var url = "https://api.magicthegathering.io/v1/cards?page=" + (clicks);
+  console.log(url);
+  makeRequest(url, requestComplete);
+}
+
+var onListHover = function(){
+  var image = document.getElementsByTagName("li").firstChild.nodeValue;
+  image.setAttribute("style", "display: block");
+}
+
+var onListOut = function(){
+  var image = document.getElementsByTagName("img");
+  image.setAttribute("style", "display: none");
+}
 
 var app = function(){
   var url = "https://api.magicthegathering.io/v1/cards?page=1";
-  var nextPageButton = document.querySelector("#nextPage");
+  var nextPage = document.querySelector("#nextPage");
   var clickTest = document.querySelector("#clickTest");
+  var backPage = document.querySelector("#backPage");
   makeRequest(url, requestComplete);
-  nextPageButton.onclick = nextPageButton;
-  clickTest.onclick = clickTest;
+  var list = document.getElementsByTagName("li");
+  nextPage.onclick = nextPageButton;
+  backPage.onclick = backPageButton;
+  clickTest.onclick = butttonTest;
+  
+  list.onmouseover = onListHover;
+  list.onmouseout = onListOut;
+  
 
 }
 
